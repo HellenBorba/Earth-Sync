ca8abb4f905fede5956f2cd968251cdef803c52c
Hellen Machado Borba <102324118+HellenBorba@users.noreply.github.com>
2025-10-24 22:53:29 -0300

    Arrumando para o código rodar
    Rodando Back e Front.

--- earthsync-server/src/index.ts
+++ earthsync-server/src/index.ts
@@ -7,5 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/history", historyRoutes);
+<<<<<<< Updated upstream
+=======
+app.use("/api/events", eventsRoutes);
+>>>>>>> Stashed changes
 
-app.listen(8080, () => console.log("✅ Server running on http://localhost:8080"));
+app.listen(5000, () => console.log("✅ Server running on http://localhost:5000"));
